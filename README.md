e4f8aa86ff80ec9b5c58dbf20f2e581a534f857c
Trevor Brooks <trevorism@gmail.com>
2026-01-10 17:27:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# memo
+![Build](https://github.com/trevorism/memo/actions/workflows/deploy.yml/badge.svg)
+![GitHub last commit](https://img.shields.io/github/last-commit/trevorism/memo)
+![GitHub language count](https://img.shields.io/github/languages/count/trevorism/memo)
+![GitHub top language](https://img.shields.io/github/languages/top/trevorism/memo)
+
+# How to build
+`gradle clean build`
---
+++ src/app/src/components/SplashPage.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      count: 0,
+      val: ''
+    }
+  },
+  mounted() {
+    axios
+      .get('api/ping')
+      .then((x) => {
+        this.val = x.data
+      })
+      .catch(() => {
+        this.val = 'Error'
+      })
+  }
+}
+</script>
+
+<script setup>
+import { VaIcon } from 'vuestic-ui'
+
+defineProps({
+  msg: String
+})
+</script>
+
+<template>
+  <div class="grid justify-items-center">
+    <h1>{{ msg }}</h1>
+    <h1>{{ val }}</h1>
+    <va-icon name="home" size="large" color="red-400"></va-icon>
+  </div>
+</template>
+
+<style scoped></style>
